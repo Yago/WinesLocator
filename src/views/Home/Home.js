b57f91617ed6d8da096520b228a94d987f7fc7d4
Yann Gouffon <github@yago-art.ch>
2018-05-26 15:41:53 +0200

    remove testing script

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -10,16 +10,16 @@ import Details from '../Details/Details';
 
 class Home extends Component {
   // Trigger redirection to test
-  componentDidUpdate(prevProps, prevState) {
-    if (this.props.api && this.props.api.data && this.props.api.data.length > 0) {
-      setTimeout(() => {
-        this.pushPage(
-          this.props.api.data.filter(item => !item.fields.parent)[6],
-          this.props.navigator
-        )
-      }, 500);
-    }
-  }
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (this.props.api && this.props.api.data && this.props.api.data.length > 0) {
+  //     setTimeout(() => {
+  //       this.pushPage(
+  //         this.props.api.data.filter(item => !item.fields.parent)[6],
+  //         this.props.navigator
+  //       )
+  //     }, 500);
+  //   }
+  // }
 
   pushPage(item, navigator) {
     navigator.pushPage({
